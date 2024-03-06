f72f69491a7d982d1719eeaf8b1eb9f4ed0cb051
Yizheng Huang <huangyz0918@gmail.com>
2024-03-06 14:08:48 -0800

    ✨ feat:

--- termax/cli/cli.py
+++ termax/cli/cli.py
@@ -1,20 +1,47 @@
+import os
 import click
 import inquirer
 
 import termax
-from termax.utils import Config, CONFIG_LLM_LIST
+from termax.utils import Config, CONFIG_LLM_LIST, CONFIG_PATH
 
 
-@click.group()
-@click.version_option(version=termax.__version__)
-def cli():
-    pass
+class DefaultCommandGroup(click.Group):
+    """allow a default command for a group"""
+
+    def command(self, *args, **kwargs):
+        default_command = kwargs.pop('default_command', False)
+        if default_command and not args:
+            kwargs['name'] = kwargs.get('name', 'termax/t')
+        decorator = super(
+            DefaultCommandGroup, self).command(*args, **kwargs)
+
+        if default_command:
+            def new_decorator(f):
+                cmd = decorator(f)
+                self.default_command = cmd.name
+                return cmd
+
+            return new_decorator
+
+        return decorator
+
+    def resolve_command(self, ctx, args):
+        try:
+            # test if the command parses
+            return super(
+                DefaultCommandGroup, self).resolve_command(ctx, args)
+        except click.UsageError:
+            # command did not parse, assume it is the default command
+            args.insert(0, self.default_command)
+            return super(
+                DefaultCommandGroup, self).resolve_command(ctx, args)
 
 
-@cli.command()
-def config():
+def build_config():
     """
-    Set up the global configuration for Termax.
+    build_config: build the configuration for Termax.
+    :return:
     """
     questions = [
         inquirer.List(
@@ -25,7 +52,6 @@ def config():
     ]
 
     answers = inquirer.prompt(questions)
-    click.echo(answers)
 
     sub_answers = None
     if answers["platform"] == "OpenAI":
@@ -49,4 +75,32 @@ def config():
 
     configuration = Config()
     configuration.write_platform(default_config, platform=platform)
-    click.echo(configuration.read())
+
+
+@click.group(cls=DefaultCommandGroup)
+@click.version_option(version=termax.__version__)
+def cli():
+    pass
+
+
+@cli.command(default_command=True)
+@click.argument('text')
+def generate(text):
+    """This function will call and generate the commands from LLM"""
+    config_dict = Config().read()
+    click.echo({"config": config_dict, "text": text})
+
+    # check the configuration available or not
+    if not os.path.exists(CONFIG_PATH):
+        click.echo("Config file not found. Running config setup...")
+        build_config()
+
+    # TODO: call the openAI API request, the parameter "text" is the input text.
+
+
+@cli.command()
+def config():
+    """
+    Set up the global configuration for Termax.
+    """
+    build_config()
