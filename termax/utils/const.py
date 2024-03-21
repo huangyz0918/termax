965f894aca22236e432eede4d0c6f59fc6445591
Yizheng Huang <huangyz0918@gmail.com>
2024-03-21 09:23:52 +0800

    🐛 fix: fixed the lint issues

--- termax/utils/const.py
+++ termax/utils/const.py
@@ -1,12 +1,12 @@
 CONFIG_SEC_GENERAL = 'general'
 CONFIG_SEC_API_KEY = 'api_key'
 CONFIG_LLM_LIST = {
-    'OpenAI' : 'gpt-3.5-turbo', 
-    'Gemini' : 'gemini-pro', 
-    'Claude' : 'claude-3-opus-20240229', 
-    'Mistral' : 'mistral-small-latest', 
-    'Qianfan' : 'ERNIE-3.5-8K', 
-    'Qianwen' : 'qwen-turbo'}
+    'OpenAI': 'gpt-3.5-turbo',
+    'Gemini': 'gemini-pro',
+    'Claude': 'claude-3-opus-20240229',
+    'Mistral': 'mistral-small-latest',
+    'Qianfan': 'ERNIE-3.5-8K',
+    'Qianwen': 'qwen-turbo'}
 
 DB_PATH = 'database'
 DB_COMMAND_HISTORY = 'history'
